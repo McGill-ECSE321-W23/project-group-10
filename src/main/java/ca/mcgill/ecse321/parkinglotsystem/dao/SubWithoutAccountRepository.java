4da61e3c862b8bce09a49f192ae350a0b3e62220
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-02-22 15:53:25 -0500

    Update SubWithoutAccountRepository.java

--- src/main/java/ca/mcgill/ecse321/parkinglotsystem/dao/SubWithoutAccountRepository.java
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/dao/SubWithoutAccountRepository.java
@@ -4,15 +4,12 @@ import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
-import ca.mcgill.ecse321.parkinglotsystem.model.MonthlyCustomer;
 import ca.mcgill.ecse321.parkinglotsystem.model.ParkingSpot;
 import ca.mcgill.ecse321.parkinglotsystem.model.SubWithoutAccount;
 
 public interface SubWithoutAccountRepository extends CrudRepository<SubWithoutAccount, Integer>{
 
-    SubWithoutAccount findSubWithoutAccountById(String id);
-
-    List<SubWithoutAccount> findSubWithoutAccountByMonthlyCustomer(MonthlyCustomer mCustomer);
+    SubWithoutAccount findSubWithoutAccountById(int id);
 
     List<SubWithoutAccount> findSubWithoutAccountByParkingSpot(ParkingSpot parkingSpot);
 
