f9b14687ac296da60b23fe16785e7ec530cf648d
Mike <xiaoming.zhang@mail.mcgill.ca>
2023-03-01 09:30:36 -0500

    Test completed not tested

---
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/dao/ReservationRepository.java
@@ -0,0 +1,26 @@
+package ca.mcgill.ecse321.parkinglotsystem.dao;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import ca.mcgill.ecse321.parkinglotsystem.model.PaymentReservation;
+import ca.mcgill.ecse321.parkinglotsystem.model.Reservation;
+import ca.mcgill.ecse321.parkinglotsystem.model.SingleReservation;
+import ca.mcgill.ecse321.parkinglotsystem.model.SubWithAccount;
+import ca.mcgill.ecse321.parkinglotsystem.model.SubWithoutAccount;
+
+public interface ReservationRepository extends CrudRepository<Reservation, Integer>{
+    
+    SingleReservation findSingleReservationById(int id);
+    SubWithAccount findSubWithAccountById(int id);
+    SubWithoutAccount findSubWithoutAccountById(int id);
+
+
+    List<Reservation> findReservationsbyDate(Date date);
+
+    Reservation findReservationByPaymentReservation(PaymentReservation paymentReservation); 
+
+    List<Reservation> findReservationsByLicenseNumber(String licenseNumber);
+}
