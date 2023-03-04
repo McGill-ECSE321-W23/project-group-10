7325290b33bd5317158b73553cb4326b5a1dfe7e
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-04 16:36:49 -0500

    Update ParkingSpotTypeRepositoryTests.java
    Removed unused import

--- src/test/java/ca/mcgill/ecse321/parkinglotsystem/dao/ParkingSpotTypeRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/parkinglotsystem/dao/ParkingSpotTypeRepositoryTests.java
@@ -1,7 +1,6 @@
 package ca.mcgill.ecse321.parkinglotsystem.dao;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
