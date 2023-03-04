8d141fb2850d550c63641dc4802b7021eb00d8ed
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-04 13:02:07 -0500

    Update PaymentReservationRepositoryTests.java
    Fixed test

--- src/test/java/ca/mcgill/ecse321/parkinglotsystem/dao/PaymentReservationRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/parkinglotsystem/dao/PaymentReservationRepositoryTests.java
@@ -8,43 +8,75 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import java.sql.Timestamp; 
 
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import ca.mcgill.ecse321.parkinglotsystem.model.ParkingSpot;
+import ca.mcgill.ecse321.parkinglotsystem.model.ParkingSpotType;
 import ca.mcgill.ecse321.parkinglotsystem.model.PaymentReservation;
-import ca.mcgill.ecse321.parkinglotsystem.model.Reservation;
+import ca.mcgill.ecse321.parkinglotsystem.model.SingleReservation;
 
 @SpringBootTest
 public class PaymentReservationRepositoryTests {
     
     @Autowired
     PaymentReservationRepository paymentReservationRepository;
-
-
-    //should I create a reservation repository or a repo for each subclass
-    //TODO: Delete this (?)
-    //@Autowired
-    //private Reservation reservation;
+    @Autowired
+    ParkingSpotTypeRepository parkingSpotTypeRepository;
+    @Autowired
+    ParkingSpotRepository parkingSpotRepository;
+    @Autowired
+    ReservationRepository reservationRepository;
 
     @AfterEach
     public void clearDatabase() {
         paymentReservationRepository.deleteAll();
-
+        reservationRepository.deleteAll();
+        parkingSpotRepository.deleteAll();
+        parkingSpotTypeRepository.deleteAll();
     }
 
     @Test
     public void testPersistAndLoadPaymentReservation() {
-
+        // Create dummy data
+        int id = 1;
         String time="2018-09-01 09:01:15"; 
         Timestamp timestamp= Timestamp.valueOf(time); 
         double amount = 70.0;
 
+        // Save objects
+        ParkingSpotType parkingSpotType = new ParkingSpotType();
+        parkingSpotType.setName("someType");
+        parkingSpotType.setFee(10);
+        parkingSpotType = parkingSpotTypeRepository.save(parkingSpotType);
+
+        ParkingSpot parkingSpot = new ParkingSpot();
+        parkingSpot.setType(parkingSpotType);
+        parkingSpot = parkingSpotRepository.save(parkingSpot);
+
+        SingleReservation reservation = new SingleReservation();
+        reservation.setDate(Date.valueOf("2018-09-01"));
+        reservation.setLicenseNumber("123");
+        reservation.setParkingTime(15);
+        reservation.setParkingSpot(parkingSpot);
+        reservation = (SingleReservation) reservationRepository.save(reservation);
+        int reservationId = reservation.getId();
 
         PaymentReservation paymentReservation = new PaymentReservation();
+        paymentReservation.setId(id);
         paymentReservation.setAmount(amount);
         paymentReservation.setDateTime(timestamp);
-
+        paymentReservation.setReservation(reservation);
         paymentReservation = paymentReservationRepository.save(paymentReservation);
-        //TODO: Add assertions
+
+        // Load objects
+        paymentReservation = paymentReservationRepository.findPaymentReservationById(id);
+        
+        assertNotNull(paymentReservation);
+        assertEquals(paymentReservation.getAmount(), amount);
+        assertEquals(paymentReservation.getDateTime(), timestamp);
+        assertEquals(paymentReservation.getReservation().getId(), reservationId);
     }
 
 }
