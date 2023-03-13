88e1348cb72f55d098b4983a3a76896083c819c1
shaun210 <78035557+shaun210@users.noreply.github.com>
2023-03-12 21:29:51 -0400

    updated HelperMethods.java
    Added method to parking spot and parking spot type instances to DTO instances

--- src/main/java/ca/mcgill/ecse321/parkinglotsystem/service/utilities/HelperMethods.java
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/service/utilities/HelperMethods.java
@@ -3,6 +3,9 @@ package ca.mcgill.ecse321.parkinglotsystem.service.utilities;
 import java.util.ArrayList;
 import java.util.List;
 
+import ca.mcgill.ecse321.parkinglotsystem.dto.*;
+import ca.mcgill.ecse321.parkinglotsystem.model.*;
+
 public class HelperMethods {
 
     /**
@@ -18,4 +21,38 @@ public class HelperMethods {
 		}
 		return resultList;
 	}
+
+    /**
+     * Helper method to convert parking spot type to a DTO
+     * @param parkingSpotType
+     * @return Dto
+     */
+    private ParkingSpotTypeDto convertParkingSpotTypeToDto(ParkingSpotType parkingSpotType) {
+
+        if (parkingSpotType == null) {
+            throw new IllegalArgumentException("There is no such parking spot type! ");
+        }
+        ParkingSpotTypeDto parkingSpotTypeDto = new ParkingSpotTypeDto();
+        parkingSpotTypeDto.setName(parkingSpotType.getName());
+        parkingSpotTypeDto.setFee(parkingSpotType.getFee());
+        return parkingSpotTypeDto;
+    }
+
+    /**
+     * Helper method to convert parking spot type to a DTO
+     * @param parkingSpot  
+     * @return Dto
+     */
+    private ParkingSpotDto convertParkingSpotToDto(ParkingSpot parkingSpot) {
+
+        if (parkingSpot == null) {
+            throw new IllegalArgumentException("There is no such parking spot type! ");
+        }
+        ParkingSpotDto parkingSpotDto = new ParkingSpotDto();
+        
+
+        parkingSpotDto.setId(parkingSpot.getId());
+        parkingSpotDto.setType(parkingSpot.getType());
+        return parkingSpotDto;
+    }
 }
