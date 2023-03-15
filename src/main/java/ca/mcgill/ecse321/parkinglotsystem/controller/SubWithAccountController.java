78f09816476ff21f57bd1c97fb1a15a6bce2d2ff
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-15 15:33:58 -0400

    Add service and controller classes for SubWithAccount

---
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/controller/SubWithAccountController.java
@@ -0,0 +1,74 @@
+package ca.mcgill.ecse321.parkinglotsystem.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import ca.mcgill.ecse321.parkinglotsystem.dto.SubWithAccountDto;
+import ca.mcgill.ecse321.parkinglotsystem.model.SubWithAccount;
+import ca.mcgill.ecse321.parkinglotsystem.service.SubWithAccountService;
+import static ca.mcgill.ecse321.parkinglotsystem.service.utilities.HelperMethods.convertSubWithAccountToDto;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/sub-with-account")
+public class SubWithAccountController {
+    
+    @Autowired
+    private SubWithAccountService subWithAccountService;
+
+    @GetMapping(value = {"", "/"})
+    public List<SubWithAccountDto> getAll() {
+        return convertSubWithAccountToDtoList(subWithAccountService.getAll());
+    }
+
+    @GetMapping(value = {"/all-by-customer/{email}", "/all-by-customer/{email}/"})
+    public List<SubWithAccountDto> getAll(
+        @PathVariable("email") String monthlyCustomerEmail) {
+        return convertSubWithAccountToDtoList(subWithAccountService.getAll(monthlyCustomerEmail));
+    }
+
+    @GetMapping(value = {"/{id}", "/{id}/"})
+    public SubWithAccountDto getSubWithAccount(@PathVariable("id") int id) {
+        return convertSubWithAccountToDto(subWithAccountService.getSubWithAccount(id));
+    }
+
+    @GetMapping(value = {"/active-by-customer/{email}", "/active-by-customer/{email}/"})
+    public SubWithAccountDto getActiveSubWithAccount(
+        @PathVariable("email") String monthlyCustomerEmail) {
+        return convertSubWithAccountToDto(subWithAccountService.getActiveSubWithAccount(monthlyCustomerEmail));
+    }
+    
+    @PostMapping(value = {"", "/"})
+    public SubWithAccountDto createSubWithAccount(
+        @RequestParam(value = "customer-email") String monthlyCustomerEmail,
+        @RequestParam(value = "parking-spot-id") int parkingSpotId) {
+        SubWithAccount sub = subWithAccountService.createSubWithAccount(monthlyCustomerEmail, parkingSpotId);
+        return convertSubWithAccountToDto(sub);
+    }
+
+    @PostMapping(value = {"/{email}", "/{email}/"})
+    public SubWithAccountDto updateSubWithAccount(
+        @PathVariable("email") String monthlyCustomerEmail) {
+        return convertSubWithAccountToDto(subWithAccountService.updateSubWithAccount(monthlyCustomerEmail));
+    }
+
+    private List<SubWithAccountDto> convertSubWithAccountToDtoList(List<SubWithAccount> subs) {
+
+        // TODO: Move this method to HelperMethods.java
+        List<SubWithAccountDto> subsDto = new ArrayList<>();
+        for (var sub : subs) {
+            subsDto.add(convertSubWithAccountToDto(sub));
+        }
+        return subsDto;
+    }
+
+}
---
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/service/SubWithAccountService.java
@@ -0,0 +1,156 @@
+package ca.mcgill.ecse321.parkinglotsystem.service;
+
+import java.sql.Date;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import ca.mcgill.ecse321.parkinglotsystem.dao.MonthlyCustomerRepository;
+import ca.mcgill.ecse321.parkinglotsystem.dao.ParkingSpotRepository;
+import ca.mcgill.ecse321.parkinglotsystem.dao.SubWithAccountRepository;
+import ca.mcgill.ecse321.parkinglotsystem.model.SubWithAccount;
+import static ca.mcgill.ecse321.parkinglotsystem.service.utilities.HelperMethods.toList;
+
+@Service
+public class SubWithAccountService {
+
+    @Autowired
+    private SubWithAccountRepository subWithAccountRepository;
+    @Autowired
+    private MonthlyCustomerRepository monthlyCustomerRepository;
+    @Autowired
+    private ParkingSpotRepository parkingSpotRepository;
+
+    /**
+     * Creates a subscription with the given monthly customer account and parking spot.
+     * 
+     * @param monthlyCustomerEmail the email of the monthly customer for whom to create the subscription
+     * @param parkingSpotId the id of the parking spot to reserve
+     * @return a DTO representing the new subscription.
+     */
+    @Transactional
+    public SubWithAccount createSubWithAccount(
+        String monthlyCustomerEmail, int parkingSpotId) {
+        
+        // Get monthly customer
+        var monthlyCustomer = monthlyCustomerRepository.findMonthlyCustomerByEmail(monthlyCustomerEmail);
+        if (monthlyCustomer == null) {
+            throw new IllegalArgumentException("The provided monthly customer email is invalid.");
+        }
+
+        // Get parking spot
+        var parkingSpot = parkingSpotRepository.findParkingSpotById(parkingSpotId);
+        if (parkingSpot == null) {
+            throw new IllegalArgumentException("The provided parking spot ID is invalid.");
+        }
+        if (!(parkingSpotId >= 2000 && parkingSpotId < 3000)){
+            throw new IllegalArgumentException("The parking spot is not available for monthly customers");
+        }
+
+        // Check whether an active subscription exists
+        for(SubWithAccount sub : subWithAccountRepository.findSubWithAccountByCustomer(monthlyCustomer)) {
+            if (isActive(sub)) {
+                throw new IllegalArgumentException("The monthly customer already has an active subscription.");
+            }
+        }
+
+        // Create & save new subscription
+        Date date = new Date((new java.util.Date()).getTime());
+        SubWithAccount subWithAccount = new SubWithAccount();
+        subWithAccount.setDate(date);
+        subWithAccount.setNbrMonths(1);
+        subWithAccount.setParkingSpot(parkingSpot);
+        subWithAccount.setCustomer(monthlyCustomer);
+        subWithAccountRepository.save(subWithAccount);
+
+        return subWithAccount;
+    }
+
+    @Transactional
+    public SubWithAccount getSubWithAccount(int id) {
+
+        var subWithAccount = subWithAccountRepository.findSubWithAccountById(id);
+        if (subWithAccount == null) {
+            throw new IllegalArgumentException("The provided subscription ID is invalid.");
+        }
+        return subWithAccount;
+    }
+
+    @Transactional
+    public SubWithAccount getActiveSubWithAccount(String monthlyCustomerEmail) {
+
+        // Get list of subscriptions
+        List<SubWithAccount> subs = getAll(monthlyCustomerEmail);
+
+        // Get current sbscription
+        SubWithAccount latestSub = subs.get(subs.size() - 1);
+        if (!isActive(latestSub)) {
+            throw new IllegalArgumentException("There is no active subscription");
+        }
+
+        return latestSub;
+    }
+
+    @Transactional
+    public List<SubWithAccount> getAll(String monthlyCustomerEmail) {
+
+        // Get monthly customer
+        var monthlyCustomer = monthlyCustomerRepository.findMonthlyCustomerByEmail(monthlyCustomerEmail);
+        if (monthlyCustomer == null) {
+            throw new IllegalArgumentException("The provided monthly customer email is invalid.");
+        }
+
+        // Get sorted list of subscriptions (from oldest to most recent)
+        List<SubWithAccount> subs = toList(subWithAccountRepository.findSubWithAccountByCustomer(monthlyCustomer));
+        if (subs.size() <= 0) {
+            throw new IllegalArgumentException("No subscription found");
+        }
+        Collections.sort(subs, Comparator.comparing(SubWithAccount::getDate));
+
+        return subs;
+    }
+
+    @Transactional
+    public List<SubWithAccount> getAll() {
+
+        // Get list of SubWithAccounts
+        List<SubWithAccount> subs = toList(subWithAccountRepository.findAll());
+        if (subs.size() <= 0) {
+            throw new IllegalArgumentException("No subscription found");
+        }
+
+        return subs;
+    }
+
+    @Transactional
+    public SubWithAccount updateSubWithAccount(String monthlyCustomerEmail) {
+
+        // Get subscription
+        SubWithAccount sub = getActiveSubWithAccount(monthlyCustomerEmail);
+
+        // Update subscription
+        sub.setNbrMonths(sub.getNbrMonths() + 1);
+        subWithAccountRepository.save(sub);
+
+        return sub;
+    }
+
+    /**
+     * Checks whether the last day of the subscription is after the current day.
+     * 
+     * @param sub
+     * @return true if the subscription is still active
+     */
+    private boolean isActive(SubWithAccount sub) {
+
+        Date date = new Date((new java.util.Date()).getTime());
+        Date lastSubDate = Date.valueOf(
+            sub.getDate().toLocalDate().plusMonths(sub.getNbrMonths()).minusDays(1));
+        return lastSubDate.after(date);
+    }
+
+}
