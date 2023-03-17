e0aea241b107f80894e2cdf547d6c865be5d6f4b
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-17 15:59:12 -0400

    SubWithAccount service javadoc & fix code
    Also created dummy method getParkingSpot()

--- src/main/java/ca/mcgill/ecse321/parkinglotsystem/service/SubWithAccountService.java
+++ src/main/java/ca/mcgill/ecse321/parkinglotsystem/service/SubWithAccountService.java
@@ -6,6 +6,7 @@ import java.util.Comparator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,6 +14,7 @@ import ca.mcgill.ecse321.parkinglotsystem.dao.SubWithAccountRepository;
 import ca.mcgill.ecse321.parkinglotsystem.model.MonthlyCustomer;
 import ca.mcgill.ecse321.parkinglotsystem.model.ParkingSpot;
 import ca.mcgill.ecse321.parkinglotsystem.model.SubWithAccount;
+import ca.mcgill.ecse321.parkinglotsystem.service.exceptions.CustomException;
 import static ca.mcgill.ecse321.parkinglotsystem.service.utilities.HelperMethods.toList;
 
 @Service
@@ -25,14 +27,14 @@ public class SubWithAccountService {
     @Autowired
     private ParkingSpotService parkingSpotService;
 
-    // TODO: Add javadoc comments
+    // TODO: Check if other service methods (MonthlyCustomer, ParkingSpot) perform input valdidation (IMPORTANT)
 
     /**
      * Creates a subscription with the given monthly customer account and parking spot.
      * 
      * @param monthlyCustomerEmail the email of the monthly customer for whom to create the subscription
      * @param parkingSpotId the id of the parking spot to reserve
-     * @return a DTO representing the new subscription.
+     * @return the new subscription
      */
     @Transactional
     public SubWithAccount createSubWithAccount(
@@ -40,13 +42,15 @@ public class SubWithAccountService {
         
         // Data validation
         if (!(parkingSpotId >= 2000 && parkingSpotId < 3000)){
-            throw new IllegalArgumentException("The parking spot is not available for monthly customers.");
+            throw new CustomException(
+                "The parking spot is not available for monthly customers.", HttpStatus.BAD_REQUEST);
         }
-        MonthlyCustomer monthlyCustomer = monthlyCustomerService.getMonthlyCustomer(monthlyCustomerEmail);
+        MonthlyCustomer monthlyCustomer = monthlyCustomerService.getMonthlyCustomerByEmail(monthlyCustomerEmail);
         ParkingSpot parkingSpot = parkingSpotService.getParkingSpot(parkingSpotId);
         for(SubWithAccount sub : subWithAccountRepository.findSubWithAccountByCustomer(monthlyCustomer)) {
             if (isActive(sub)) {
-                throw new IllegalArgumentException("The monthly customer already has an active subscription.");
+                throw new CustomException(
+                    "The monthly customer already has an active subscription.", HttpStatus.BAD_REQUEST);
             }
         }
 
@@ -62,64 +66,85 @@ public class SubWithAccountService {
         return subWithAccount;
     }
 
+    /**
+     * Gets a subcription with the given ID.
+     * 
+     * @param id the ID of the subscription
+     * @return a subscription
+     */
     @Transactional
     public SubWithAccount getSubWithAccount(int id) {
 
         var subWithAccount = subWithAccountRepository.findSubWithAccountById(id);
         if (subWithAccount == null) {
-            throw new IllegalArgumentException("The provided subscription ID is invalid.");
+            throw new CustomException("The provided subscription ID is invalid.", HttpStatus.NOT_FOUND);
         }
+
         return subWithAccount;
     }
 
+    /**
+     * Gets the active subscription of the given monthly customer.
+     * 
+     * @param monthlyCustomerEmail the email of the monthly customer
+     * @return the active subscription. Throws a CustomException if no active subscription is found.
+     */
     @Transactional
     public SubWithAccount getActiveSubWithAccount(String monthlyCustomerEmail) {
 
-        // Get list of subscriptions
         List<SubWithAccount> subs = getAll(monthlyCustomerEmail);
-
-        // Get current sbscription
+        if(subs.size() <= 0) {
+            throw new CustomException("There is no active subscription", HttpStatus.NOT_FOUND);
+        }
         SubWithAccount latestSub = subs.get(subs.size() - 1);
         if (!isActive(latestSub)) {
-            throw new IllegalArgumentException("There is no active subscription");
+            throw new CustomException("There is no active subscription", HttpStatus.NOT_FOUND);
         }
 
         return latestSub;
     }
 
+    /**
+     * Gets all subscriptions of the given monthly customer.
+     * 
+     * @param monthlyCustomerEmail the email of the monthly customer
+     * @return the list of subscriptions sorted by date (ascending).
+     */
     @Transactional
     public List<SubWithAccount> getAll(String monthlyCustomerEmail) {
 
-        // Get sorted list of subscriptions (from oldest to most recent)
-        MonthlyCustomer monthlyCustomer = monthlyCustomerService.getMonthlyCustomer(monthlyCustomerEmail);
+        MonthlyCustomer monthlyCustomer = monthlyCustomerService.getMonthlyCustomerByEmail(monthlyCustomerEmail);
         List<SubWithAccount> subs = toList(subWithAccountRepository.findSubWithAccountByCustomer(monthlyCustomer));
-        if (subs.size() <= 0) {
-            throw new IllegalArgumentException("No subscription found");
-        }
         Collections.sort(subs, Comparator.comparing(SubWithAccount::getDate));
 
         return subs;
     }
 
+    /**
+     * Gets all subcriptions.
+     * 
+     * @return the list of subscriptions sorted by date (ascending).
+     */
     @Transactional
     public List<SubWithAccount> getAll() {
 
-        // Get list of SubWithAccounts
         List<SubWithAccount> subs = toList(subWithAccountRepository.findAll());
-        if (subs.size() <= 0) {
-            throw new IllegalArgumentException("No subscription found");
-        }
+        Collections.sort(subs, Comparator.comparing(SubWithAccount::getDate));
 
         return subs;
     }
 
+    /**
+     * Updates the active subscription of the given monthly customer
+     * by incrementing the number of months by one.
+     * 
+     * @param monthlyCustomerEmail the email of the monthly customer
+     * @return the updated subscription
+     */
     @Transactional
     public SubWithAccount updateSubWithAccount(String monthlyCustomerEmail) {
 
-        // Get subscription
         SubWithAccount sub = getActiveSubWithAccount(monthlyCustomerEmail);
-
-        // Update subscription
         sub.setNbrMonths(sub.getNbrMonths() + 1);
         subWithAccountRepository.save(sub);
 
@@ -130,13 +155,12 @@ public class SubWithAccountService {
      * Checks whether the last day of the subscription is after the current day.
      * 
      * @param sub
-     * @return true if the subscription is still active
+     * @return true if the subscription is still active.
      */
     private boolean isActive(SubWithAccount sub) {
 
         Date date = new Date((new java.util.Date()).getTime());
-        Date lastSubDate = Date.valueOf(
-            sub.getDate().toLocalDate().plusMonths(sub.getNbrMonths()).minusDays(1));
+        Date lastSubDate = Date.valueOf(sub.getDate().toLocalDate().plusMonths(sub.getNbrMonths()).minusDays(1));
         return lastSubDate.after(date);
     }
 
