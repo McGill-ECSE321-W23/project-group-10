9aa6517f0249f5392ebc0f704afcf231cc866093
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-04-04 16:50:31 -0400

    Add navbar component

--- ParkingLotSystem-Frontend/src/App.vue
+++ ParkingLotSystem-Frontend/src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
     <router-view></router-view>
   </div>
 </template>
@@ -12,12 +11,28 @@ export default {
 </script>
 
 <style>
+html, body {
+  margin:0;
+  height:100%;
+  box-sizing: border-box;
+}
+
+body {
+  background-image: linear-gradient(135deg, #dbe5f3 0%, #ffffff 100%);
+  background-size: cover;
+  background-attachment: fixed;
+  background-position: center;
+}
+
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: 'Poppins', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+.content {
   margin-top: 60px;
 }
 </style>
