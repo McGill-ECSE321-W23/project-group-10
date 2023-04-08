3e28d886be99875a4c02d34b6d9318e2745c0b0d
Mike <xiaoming.zhang@mail.mcgill.ca>
2023-04-08 16:36:30 -0400

    Scripts

--- ParkingLotSystem-Frontend/src/scripts/AdminSetting.js
+++ ParkingLotSystem-Frontend/src/scripts/AdminSetting.js
@@ -1,5 +1,7 @@
 import NavBar from '@/components/NavBar.vue'
-import axios from 'axios'
+import axios, { Axios } from 'axios'
+var config = require('../../config')
+
 
 var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
 var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
@@ -12,6 +14,8 @@ var AXIOS = axios.create({
     name: "admin-settings",
     data() {
         return{
+            parkingSpotType: [],
+            service: [],
             navItems: [
                 { text: "Dashboard", href: "#"},
                 { text: "Settings", href: "#"},
@@ -24,10 +28,58 @@ var AXIOS = axios.create({
             showError: false
         }
     },
-    created() {
-        this.refresh();
-    },
+    
     methods: {
-        
-    }
+        addService: function(){
+            try{
+                var serviceName = document.getElementById("typeName");
+                var serviceFee = document.getElementById("serviceFee");
+
+                let service = AXIOS.post(`/api/service/${serviceName}`, {serviceFee});
+            } catch(e){
+                this.error(e);
+            }
+        },
+        setFee: function() {
+            try{
+                var type = document.getElementById("type");
+                var fee = document.getElementById("parkingFee");
+
+                let parkingFee = AXIOS.put(`/api/parking-spot-type/${type}/`, {fee});
+            }catch(e){
+                this.error(e);
+            }
+        },
+
+        setHours: function() {
+            try{
+                var startHour = document.getElementById("startHour");
+                var closeHour = document.getElementById("closeHour");
+                var startTime = document.getElementById("startTime");
+                var closeTime = document.getElementById("closeTime");
+                if (startTime == "PM"){
+                    startHour+=12;
+                }
+                if (closeTime == "PM"){
+                    closeHour+=12;
+                }
+
+                let openingHours = AXIOS.put(`/api/parking-lot-system/${id}/`, {startHour, closeHour});
+            }catch(e){
+                this.error(e);
+            }
+        },
+
+        error(e) {
+            if(e.hasOwnProperty("response")) {
+              this.errorMessage = e.response.data.message;
+            }
+            else {
+              this.errorMessage = e.message;
+            }
+            this.showError = true;
+          }
+    },
+    components: {NavBar}
+
  }
