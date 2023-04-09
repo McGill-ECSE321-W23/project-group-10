529a5247179388b3b290648835afe835751fd4ad
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-04-08 20:36:41 -0400

    Fix profile pages

--- ParkingLotSystem-Frontend/src/components/EditProfileEmployee.vue
+++ ParkingLotSystem-Frontend/src/components/EditProfileEmployee.vue
@@ -6,27 +6,27 @@
       :username="username"
     />
     <b-alert v-model="showError" variant="danger" dismissible>Error: {{ errorMessage }}</b-alert>
-  </div>
-  <div class="content">
-    <h2>Edit Your Profile (Employee)</h2>
-    <div>
-      <label for="name">Name: </label>
-      <input type="text" id="name" v-model="employee.name" placeholder="Enter New Name">
+    <div class="content">
+      <h2>Edit Your Profile (Employee)</h2>
+      <div>
+        <label for="name">Name: </label>
+        <input type="text" id="name" v-model="employee.name" placeholder="Enter New Name">
+      </div>
+      <div>
+        <label for="name">Email: </label>
+        <input type="text" id="email" v-model="employee.email" placeholder="Enter New Email">
+      </div>
+      <div>
+        <label for="name">Phone Number: </label>
+        <input type="text" id="phoneNumber" v-model="employee.phone" placeholder="Enter New Phone Number">
+      </div>
+      <div>
+        <label for="name">Password: </label>
+        <input type="text" id="password" v-model="employee.password" placeholder="Enter New Password">
+      </div>
+      <div class="button"><button v-on:click="updateEmployee">Update</button></div>
+  
     </div>
-    <div>
-      <label for="name">Email: </label>
-      <input type="text" id="email" v-model="employee.email" placeholder="Enter New Email">
-    </div>
-    <div>
-      <label for="name">Phone Number: </label>
-      <input type="text" id="phoneNumber" v-model="employee.phone" placeholder="Enter New Phone Number">
-    </div>
-    <div>
-      <label for="name">Password: </label>
-      <input type="text" id="password" v-model="employee.password" placeholder="Enter New Password">
-    </div>
-    <div class="button"><button v-on:click="updateEmployee">Update</button></div>
-
   </div>
 </template>
 
--- ParkingLotSystem-Frontend/src/components/EditProfileMonthlyCustomer.vue
+++ ParkingLotSystem-Frontend/src/components/EditProfileMonthlyCustomer.vue
@@ -6,31 +6,31 @@
       :username="username"
     />
     <b-alert v-model="showError" variant="danger" dismissible>Error: {{ errorMessage }}</b-alert>
-  </div>
-  <div class="content">
-    <h2>Edit Your Profile (Monthly Customer)</h2>
-    <div>
-      <label for="name">Name: </label>
-      <input type="text" id="name" v-model="monthlyCustomer.name" placeholder="Enter New Name">
+    <div class="content">
+      <h2>Edit Your Profile (Monthly Customer)</h2>
+      <div>
+        <label for="name">Name: </label>
+        <input type="text" id="name" v-model="monthlyCustomer.name" placeholder="Enter New Name">
+      </div>
+      <div>
+        <label for="name">Email: </label>
+        <input type="text" id="email" v-model="monthlyCustomer.email" placeholder="Enter New Email">
+      </div>
+      <div>
+        <label for="name">Phone Number: </label>
+        <input type="text" id="phoneNumber" v-model="monthlyCustomer.phone" placeholder="Enter New Phone Number">
+      </div>
+      <div>
+        <label for="name">Password: </label>
+        <input type="text" id="password" v-model="monthlyCustomer.password" placeholder="Enter New Password">
+      </div>
+      <div>
+        <label for="name">License Number: </label>
+        <input type="text" id="licenseNumber" v-model="monthlyCustomer.licenseNumber"
+               placeholder="Enter New License Number">
+      </div>
+      <div class="button"><button v-on:click="updateMonthlyCustomer">Update</button></div>
     </div>
-    <div>
-      <label for="name">Email: </label>
-      <input type="text" id="email" v-model="monthlyCustomer.email" placeholder="Enter New Email">
-    </div>
-    <div>
-      <label for="name">Phone Number: </label>
-      <input type="text" id="phoneNumber" v-model="monthlyCustomer.phone" placeholder="Enter New Phone Number">
-    </div>
-    <div>
-      <label for="name">Password: </label>
-      <input type="text" id="password" v-model="monthlyCustomer.password" placeholder="Enter New Password">
-    </div>
-    <div>
-      <label for="name">License Number: </label>
-      <input type="text" id="licenseNumber" v-model="monthlyCustomer.licenseNumber"
-             placeholder="Enter New License Number">
-    </div>
-    <div class="button"><button v-on:click="updateMonthlyCustomer">Update</button></div>
   </div>
 
 </template>
