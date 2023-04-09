eebc627b1c8693811f34eaabda0ce1524b80f543
Mike <xiaoming.zhang@mail.mcgill.ca>
2023-04-08 20:22:41 -0400

    Welcome Page and script

--- ParkingLotSystem-Frontend/src/router/index.js
+++ ParkingLotSystem-Frontend/src/router/index.js
@@ -4,6 +4,7 @@ import Hello from '@/components/Hello'
 import ServicesAdmin from '@/components/ServicesAdmin.vue'
 import ServicesCustomer from '@/components/ServicesCustomer.vue'
 import AdminSettings from '@/components/AdminSettings.vue'
+import Welcome from '@/components/Welcome.vue'
 
 Vue.use(Router)
 
@@ -28,6 +29,11 @@ export default new Router({
       path: '/admin-settings',
       name: "AdminSettings",
       component: AdminSettings
+    },
+    {
+      path: '/welcome',
+      name: "Welcome",
+      component: Welcome
     }
   ]
 })
