cb66a47f9e17025103df6d6877e4152411fd9b4e
Louis896 <weiheng.xiao@mail.mcgill.ca>
2023-04-08 23:56:20 -0400

    reservation

--- ParkingLotSystem-Frontend/src/components/Guest.vue
+++ ParkingLotSystem-Frontend/src/components/Guest.vue
@@ -7,17 +7,49 @@
         />
         <b-alert v-model="showError" variant="danger" dismissible>Error: {{ errorMessage }}</b-alert>
         <div class="content">
-            <h2>Welcome, Customer: {{licenseNumber}}</h2>
+            <h2>Welcome, Customer !</h2>
+                <label class="body-label">
+                    License Number:
+                    <input type="text" v-model="licenseNumber" style="width: 100px;height:28px"/>
+                </label>
         </div>
 
-        <template>
-            <div>
-                <label v-for="(option, index) in options" :key="index"  class="margin-right">
-                    <input type="radio" :value="option" v-model="selectedOption">
-                    {{ option }}
-                </label>
+        <div class="radio-button-container">
+            <label v-for="(option, index) in options" :key="index"  class="body-label">
+                <input type="radio" :value="option" v-model="selectedOption">
+                {{ option }}
+            </label>
+        </div>
+
+        <div v-if="selectedOption=='For Service'">
+            <h2>Hi</h2>
+        </div>
+
+        <div v-if="selectedOption=='For Reservation'" >
+            <label class="body-label">
+                Parking Spot Number:
+                <input type="text" v-model="parkingSpotNumber" style="width: 100px;height:28px">
+            </label>
+            <div class="row">
+                <label class="body-label" for="dropdown_hour1">Parking Timespan:</label>
+                <select id="dropdown_hour1" v-model="reservation_hour" style="width: 80x;height:28px">
+                    <option value="">-Hour-</option>
+                    <option v-for="hour in hours" :key="hour.value" :value="hour.value">{{ hour.label }}</option>
+                </select>
+                <label class="body-label"> : </label>
+                <select id="dropdown_minute1" v-model="reservation_minute" style="width: 80x;height:28px">
+                    <option value="">-Min-</option>
+                    <option v-for="minute in minutes" :key="minute.value" :value="minute.value">{{ minute.label }}</option>
+                </select>
             </div>
-        </template>
+                <div>
+                    <button @click="reservation_submit" class="submit-button">Proceed Reservation</button>
+                </div>
+            </div>
+
+        <div v-if="selectedOption=='For Subscription'">
+            <h2>Hi</h2>
+        </div>
 
     </div>
 </template>
@@ -25,28 +57,26 @@
 <script src="@/scripts/Guest.js"></script>
 
 <style scoped>
-  h3 {
-    margin-bottom: 30px;
+  .row {
+    display: flex;
+    align-items: center;
+    justify-content:center;
   }
-  .card {
-    min-width: 30%;
-    max-width: 100%;
-    margin-bottom: 30px;
-  }
-  .form {
-    justify-content: center;
-    margin-bottom: 30px;
-  }
-  .underline-button {
-    border: none;
-    color: #007aff;
-    text-decoration: underline;
-    background-color: transparent;
-    cursor: pointer;
+  .body-label{
     font-size: 20px;
-    font-family: 20px;
+    margin-right: 10px;
+    margin-left: 10px;
+    margin-top: 5px;
   }
-  .margin-right {
-    margin-right: 20px; /* or any other value */
+  .input-box{
+    width: 10px;
+    height: 10px;
+  }
+  .submit-button{
+    margin-top: 10px;
+    width: 300px;
+  }
+  .radio-button-container{
+    margin-bottom: 20px;
   }
 </style>
