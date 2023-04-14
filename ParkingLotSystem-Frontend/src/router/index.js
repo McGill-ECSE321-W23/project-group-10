4b2bc22532892ebbd181f64dac22e716a2f69638
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-04-13 23:50:51 -0400

    Update index.js

--- ParkingLotSystem-Frontend/src/router/index.js
+++ ParkingLotSystem-Frontend/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello' // TODO: Remove and delete page
 import CreateParkingSpotType from '@/components/CreateParkingSpotType.vue'
 import ServicesAdmin from '@/components/ServicesAdmin.vue'
 import ServicesCustomer from '@/components/ServicesCustomer.vue'
