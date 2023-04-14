3ca8ed97fc36a4015d3afbde946fa58850bc7198
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-04-13 23:48:41 -0400

    Formatting

--- ParkingLotSystem-Frontend/src/scripts/CreateParkingSpot.js
+++ ParkingLotSystem-Frontend/src/scripts/CreateParkingSpot.js
@@ -1,44 +1,48 @@
+import axios from "axios";
+var config = require("../../config");
 
-import axios from 'axios'
-var config = require('../../config')
-
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
 
 export default {
-    name: 'parking-spot',
-    data() {
-      return {
-        parkingSpot: {
-            id: '',
-            type: ''
-        }
-
+  name: "parking-spot",
+  data() {
+    return {
+      parkingSpot: {
+        id: "",
+        type: ""
       }
-    },
-    methods: {
-      /** Create a parking spot with spot id and parking spot type. */
-        async createParkingSpot() {
-            try {
-                console.log('Creating Parking Spot: ' + this.parkingSpot.id + ' ' + this.parkingSpot.type)
-                let response = await AXIOS.post(
-                    `/api/parking-spot/${this.parkingSpot.id}`,
-                    {},
-                    {
-                        params: { parkingSpotTypeName: this.parkingSpot.type},
-                        headers: { token: localStorage.getItem("token") }
-                    }
-                )
-                console.log('Created Parking Spot:', response.data);
-                console.log(response.data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
+    };
+  },
+  methods: {
+    /** Create a parking spot with spot id and parking spot type. */
+    async createParkingSpot() {
+      try {
+        console.log(
+          "Creating Parking Spot: " +
+            this.parkingSpot.id +
+            " " +
+            this.parkingSpot.type
+        );
+        let response = await AXIOS.post(
+          `/api/parking-spot/${this.parkingSpot.id}`,
+          {},
+          {
+            params: { parkingSpotTypeName: this.parkingSpot.type },
+            headers: { token: localStorage.getItem("token") }
+          }
+        );
+        console.log("Created Parking Spot:", response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
-}
+  }
+};
--- ParkingLotSystem-Frontend/src/scripts/CreateParkingSpotType.js
+++ ParkingLotSystem-Frontend/src/scripts/CreateParkingSpotType.js
@@ -1,43 +1,48 @@
-import NavBar from '@/components/NavBar.vue'
-import axios from 'axios'
-var config = require('../../config')
+import NavBar from "@/components/NavBar.vue";
+import axios from "axios";
+var config = require("../../config");
 
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
 
 export default {
-    name: 'parking-spot-type',
-    data() {
-      return {
-        parkingSpotType: {
-            typeName: '',
-            fee: ''
-        }
+  name: "parking-spot-type",
+  data() {
+    return {
+      parkingSpotType: {
+        typeName: "",
+        fee: ""
       }
-    },
-    methods: {
-      /** Create a parking spot type with charge fee and parking spot type name. */
-        async createParkingSpotType() {
-            try {
-                console.log('Creating Parking Spot Type: ' + this.parkingSpotType.typeName + ' ' + this.parkingSpotType.fee)
-                let response = await AXIOS.post(
-                    `/api/parking-spot-type/${this.parkingSpotType.typeName}`,
-                    {},
-                    {
-                        params: { fee: this.parkingSpotType.fee},
-                        headers: { token: localStorage.getItem("token") }
-                    }
-                )
-                console.log('Created Parking Spot Type:', response.data);
-            } catch (error) {
-                console.log(error)
-            }
-        }
+    };
+  },
+  methods: {
+    /** Create a parking spot type with charge fee and parking spot type name. */
+    async createParkingSpotType() {
+      try {
+        console.log(
+          "Creating Parking Spot Type: " +
+            this.parkingSpotType.typeName +
+            " " +
+            this.parkingSpotType.fee
+        );
+        let response = await AXIOS.post(
+          `/api/parking-spot-type/${this.parkingSpotType.typeName}`,
+          {},
+          {
+            params: { fee: this.parkingSpotType.fee },
+            headers: { token: localStorage.getItem("token") }
+          }
+        );
+        console.log("Created Parking Spot Type:", response.data);
+      } catch (error) {
+        console.log(error);
+      }
     }
-
   }
+};
