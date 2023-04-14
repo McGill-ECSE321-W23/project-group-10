a77970d19d54fdb3325917d0e6712cde2bcbf93a
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-04-13 20:42:21 -0400

    Styling and formatting

--- ParkingLotSystem-Frontend/src/components/CreateMonthlyCustomerReservationPage.vue
+++ ParkingLotSystem-Frontend/src/components/CreateMonthlyCustomerReservationPage.vue
@@ -3,7 +3,7 @@
     <NavBar activeNav="create-customer-reservation"/>
     <b-alert v-model="showError" variant="danger" dismissible>Error: {{ errorMessage }}</b-alert>
     <div class="content">
-      <p>Please select the reservation type:</p>
+      <h2>Create New Subscription:</h2>
       <input type="radio" id="sub-with-account" name="reservation-type" value="subWithAccount" v-model="reservationType">
       <label for="sub-with-account">subscription with account</label><br>
       <input type="radio" id="sub-without-account" name="reservation-type" value="subWithoutAccount" v-model="reservationType">
@@ -11,22 +11,22 @@
       
       <label>License number:</label>
       <div v-if="reservationType === 'subWithAccount'">
-        <input type="email" id="monthly-customer-email" v-model="monthlyCustomerEmail" placeholder="Enter monthly customer email">
+        <input type="email" class="mb-2" v-model="monthlyCustomerEmail" placeholder="Enter monthly customer email">
       </div>
   
       <div v-if="reservationType === 'subWithoutAccount'">
-        <input type="text" id="license-number" v-model="licenseNumber" placeholder="Enter license number">
+        <input type="text" class="mb-2" v-model="licenseNumber" placeholder="Enter license number">
       </div>
 
       <label>Parking spot:</label><br>
       <b-form-select 
-        class="custom-width mx-auto"
+        class="custom-width mx-auto mb-3"
         v-model="selectedSpot" 
         :options="parkingSpots" 
         :select-size="6">
       </b-form-select><br>
   
-      <button v-on:click="createReservation">Submit</button>
+      <b-button v-on:click="createReservation">Submit</b-button>
     </div>
   </div>
 </template>
@@ -34,15 +34,6 @@
 <script src="@/scripts/CreateMonthlyCustomerReservationPage.js"> </script>
 
 <style scoped>
-.table {
-
-  height: 200px;
-  width: 200px;
-  overflow-y: scroll;
-  border-collapse: collapse;
-  margin: 0 auto;
-}
-
 td,
 th {
   padding: 8px;
--- ParkingLotSystem-Frontend/src/scripts/AdminSettings.js
+++ ParkingLotSystem-Frontend/src/scripts/AdminSettings.js
@@ -1,183 +1,184 @@
-import NavBar from '@/components/NavBar.vue'
-import axios from 'axios'
-var config = require('../../config')
+import NavBar from "@/components/NavBar.vue";
+import axios from "axios";
+var config = require("../../config");
 
-var frontendUrl = 'http://' + config.dev.host + ':' + config.dev.port
-var backendUrl = 'http://' + config.dev.backendHost + ':' + config.dev.backendPort
+var frontendUrl = "http://" + config.dev.host + ":" + config.dev.port;
+var backendUrl =
+  "http://" + config.dev.backendHost + ":" + config.dev.backendPort;
 
 var AXIOS = axios.create({
   baseURL: backendUrl,
-  headers: { 'Access-Control-Allow-Origin': frontendUrl }
-})
+  headers: { "Access-Control-Allow-Origin": frontendUrl }
+});
 
- export default {
-    name: "admin-settings",
-    data() {
-        return{
-            // Opening hours variables
-            openTime: null,
-            closeTime: null,
+export default {
+  name: "admin-settings",
+  data() {
+    return {
+      // Opening hours variables
+      openTime: null,
+      closeTime: null,
 
-            // Parking fees variables
-            spotTypes: [],
-            selectedSpotType: null,
-            spotTypeFee: 0,
+      // Parking fees variables
+      spotTypes: [],
+      selectedSpotType: null,
+      spotTypeFee: 0,
 
-            // Available services variables
-            services: [],
-            selectedService: null,
-            serviceDesc: "",
-            serviceFee: 0,
+      // Available services variables
+      services: [],
+      selectedService: null,
+      serviceDesc: "",
+      serviceFee: 0,
 
-            // Error variables
-            errorMessage: "",
-            showError: false
+      // Error variables
+      errorMessage: "",
+      showError: false
+    };
+  },
+
+  created() {
+    this.refresh();
+  },
+
+  methods: {
+    /** Updates the description and price fields. Resets fields if no service is selected. */
+    async getServiceInfo() {
+      if (this.selectedService) {
+        try {
+          let response = await AXIOS.get(
+            `/api/service/${this.selectedService}`
+          );
+          let service = response.data;
+          this.serviceDesc = service.description;
+          this.serviceFee = service.price;
+        } catch (e) {
+          this.error(e);
         }
+      } else {
+        this.serviceDesc = "";
+        this.serviceFee = 0;
+      }
     },
-
-    created() {
+    /** Updates the selected service. Creates a service if no service is selected. */
+    async saveService() {
+      try {
+        if (this.selectedService) {
+          await AXIOS.put(
+            `/api/service/${this.serviceDesc}`,
+            {},
+            { params: { price: this.serviceFee }, headers: { token: "dev" } }
+          );
+        } else {
+          await AXIOS.post(
+            `/api/service/${this.serviceDesc}`,
+            {},
+            { params: { price: this.serviceFee }, headers: { token: "dev" } }
+          );
+        }
+      } catch (e) {
+        this.error(e);
+      }
+      this.refresh();
+    },
+    /** Deletes a service. */
+    async deleteService() {
+      try {
+        await AXIOS.delete(
+          `/api/service/${this.selectedService}`,
+          {},
+          { headers: { token: "dev" } }
+        );
+      } catch (e) {
+        this.error(e);
+      }
+      this.refresh();
+    },
+    /** Gets the fee for the parking spot type. */
+    async getSpotTypeInfo() {
+      if (this.selectedSpotType) {
+        try {
+          let response = await AXIOS.get(
+            `/api/parking-spot-type/${this.selectedSpotType}`
+          );
+          let spotType = response.data;
+          this.spotTypeFee = spotType.fee;
+        } catch (e) {
+          this.error(e);
+        }
+      } else {
+        this.spotTypeFee = 0;
+      }
+    },
+    /** Sets the fee of the selected parking spot type. */
+    async setFee() {
+      try {
+        await AXIOS.put(
+          `/api/parking-spot-type/${this.selectedSpotType}`,
+          {},
+          {
+            params: { fee: this.spotTypeFee },
+            headers: { token: localStorage.getItem("token") }
+          }
+        );
         this.refresh();
+      } catch (e) {
+        this.error(e);
+      }
     },
-    
-    methods: {
-        /** Updates the description and price fields. Resets fields if no service is selected. */
-        async getServiceInfo() {
-            if(this.selectedService) {
-                try {
-                    let response = await AXIOS.get(`/api/service/${this.selectedService}`);
-                    let service = response.data;
-                    this.serviceDesc = service.description;
-                    this.serviceFee = service.price;
-                } catch(e) {
-                    this.error(e);
-                }
-            }
-            else {
-                this.serviceDesc = "";
-                this.serviceFee = 0;
-            }
-        },
-        /** Updates the selected service. Creates a service if no service is selected. */
-        async saveService(){
-            try{
-                if(this.selectedService) {
-                    await AXIOS.put(
-                        `/api/service/${this.serviceDesc}`,
-                        {},
-                        {params: {price: this.serviceFee}, headers: {token: "dev"}}
-                    );
-                } else {
-                    await AXIOS.post(
-                        `/api/service/${this.serviceDesc}`,
-                        {},
-                        {params: {price: this.serviceFee}, headers: {token: "dev"}}
-                    );
-                }
-            } catch(e){
-                this.error(e);
-            }
-            this.refresh();
-        },
-        /** Deletes a service. */
-        async deleteService() {
-            try{
-                await AXIOS.delete(
-                    `/api/service/${this.selectedService}`,
-                    {},
-                    {headers: {token: "dev"}}
-                );
-            } catch(e){
-                this.error(e);
-            }
-            this.refresh();
-        },
-        /** Gets the fee for the parking spot type. */
-        async getSpotTypeInfo() {
-            if(this.selectedSpotType) {
-                try {
-                    let response = await AXIOS.get(`/api/parking-spot-type/${this.selectedSpotType}`);
-                    let spotType = response.data;
-                    this.spotTypeFee = spotType.fee;
-                } catch(e) {
-                    this.error(e);
-                }
-            }
-            else {
-                this.spotTypeFee = 0;
-            }
-        },
-        /** Sets the fee of the selected parking spot type. */
-        async setFee() {
-            try {
-                await AXIOS.put(
-                    `/api/parking-spot-type/${this.selectedSpotType}`,
-                    {},
-                    {
-                        params: { fee: this.spotTypeFee },
-                        headers: { token: localStorage.getItem("token")}
-                    }
-                );
-                this.refresh();
-            } catch(e) {
-                this.error(e);
-            }
-        },
-        /** Sets the open time & close time of the parking lot. */
-        async setHours() {
-            try {
-                await AXIOS.put(
-                    "/api/parking-lot-system/0",
-                    {},
-                    {
-                        params: { openTime: this.openTime, closeTime: this.closeTime },
-                        headers: { token: localStorage.getItem("token")}
-                    }
-                );
-            } catch(e) {
-                this.error(e);
-            }
-        },
-        /** Gets the data from the database and updates relevant fields. */
-        async refresh() {
-            try {
-                // Get parking lot system settings
-                let response = await AXIOS.get("/api/parking-lot-system/0");
-                let pls = response.data;
-                this.openTime = pls.openTime;
-                this.closeTime = pls.closeTime;
-
-                // Get parking fees
-                response = await AXIOS.get("/api/parking-spot-type");
-                this.spotTypes = response.data.map(spotType => {
-                    return {
-                    value: spotType.name,
-                    text: `${spotType.name}: $${spotType.fee} CAD`
-                    }
-                });
-
-                // Get services
-                response = await AXIOS.get("/api/service");
-                this.services = response.data.map(service => {
-                    return {
-                    value: service.description,
-                    text: `${service.description}: $${service.price} CAD`
-                    }
-                });
-            } catch(e) {
-                this.error(e);
-            }
-        },
-        /** Displays the error message. */
-        error(e) {
-            if(e.hasOwnProperty("response")) {
-              this.errorMessage = e.response.data.message;
-            }
-            else {
-              this.errorMessage = e.message;
-            }
-            this.showError = true;
-        }
+    /** Sets the open time & close time of the parking lot. */
+    async setHours() {
+      try {
+        await AXIOS.put(
+          "/api/parking-lot-system/0",
+          {},
+          {
+            params: { openTime: this.openTime, closeTime: this.closeTime },
+            headers: { token: localStorage.getItem("token") }
+          }
+        );
+      } catch (e) {
+        this.error(e);
+      }
     },
-    components: {NavBar}
+    /** Gets the data from the database and updates relevant fields. */
+    async refresh() {
+      try {
+        // Get parking lot system settings
+        let response = await AXIOS.get("/api/parking-lot-system/0");
+        let pls = response.data;
+        this.openTime = pls.openTime;
+        this.closeTime = pls.closeTime;
 
- }
+        // Get parking fees
+        response = await AXIOS.get("/api/parking-spot-type");
+        this.spotTypes = response.data.map(spotType => {
+          return {
+            value: spotType.name,
+            text: `${spotType.name}: $${spotType.fee} CAD`
+          };
+        });
+
+        // Get services
+        response = await AXIOS.get("/api/service");
+        this.services = response.data.map(service => {
+          return {
+            value: service.description,
+            text: `${service.description}: $${service.price} CAD`
+          };
+        });
+      } catch (e) {
+        this.error(e);
+      }
+    },
+    /** Displays the error message. */
+    error(e) {
+      if (e.hasOwnProperty("response")) {
+        this.errorMessage = e.response.data.message;
+      } else {
+        this.errorMessage = e.message;
+      }
+      this.showError = true;
+    }
+  },
+  components: { NavBar }
+};
